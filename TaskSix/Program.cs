6bfccef3a254f228724c210f4de5d74a496794c7
solfire1994 <solfire1994@gmail.com>
2022-08-08 12:04:49 +0300

    Commit

--- TaskSix/Program.cs
+++ TaskSix/Program.cs
@@ -5,7 +5,7 @@ namespace TaskSix
 {
     internal class Program
     {
-        private const string Path = @"C:\SolF\VS\Repos\TaskSix\Text.txt";
+        private const string Path = @"Text.txt";
         static void Main(string[] args)
         {
             Console.WriteLine("Добрый день, что вы хотите сделать? " +
@@ -14,10 +14,8 @@ namespace TaskSix
                 "\nЕсли хотите закончить работу с модулем, то введите цифру 0");
             byte argument = byte.Parse(Console.ReadLine());
             Console.Clear();
-            if (argument == 1)
-            {
-                Print();
-            }
+            bool fileExists = File.Exists(Path);
+            if (argument == 1) if (fileExists) Print(); else Console.WriteLine("Список сотрудников пуст");
             else if (argument == 2)
             {
                 Console.WriteLine("Данные какого количества новых сотрудников вы хотите ввести?");
@@ -25,16 +23,13 @@ namespace TaskSix
                 string[] newWorkerArr = new string[argument2];
                 for (int i = 0; i < newWorkerArr.Length; i++)
                 {
-                    newWorkerArr[i] = GetString(i);
+                    newWorkerArr[i] = GetString(i, fileExists);
                     Console.WriteLine(newWorkerArr[i]);
                 }
-                WriterFile(newWorkerArr);
+                WriterFile(newWorkerArr, fileExists);
 
             }
-            else if (argument == 0)
-            {
-                Console.WriteLine("Хорошего Вам дня =)");
-            }
+            else if (argument == 0) Console.WriteLine("Хорошего Вам дня =)");
             else
             {
                 Console.WriteLine("Вы ввели некорректные данные, повторите ввод");
@@ -46,10 +41,10 @@ namespace TaskSix
         /// Метод для получения строки с данными нового сотрудника
         /// </summary>
         /// <returns>Строка с данными сотрудника</returns>
-        static string GetString(int i)
+        static string GetString(int i, bool fileExists)
         {
             int id;
-            if (File.Exists(Path))
+            if (fileExists)
             {
                 string[] arr = File.ReadAllLines(Path);
                 if (arr.Length > 0)
@@ -57,15 +52,9 @@ namespace TaskSix
                     string [] brr = arr[^1].Split('#');
                     id = int.Parse(brr[0]) + 1 + i;
                 }
-                else
-                {
-                    id = 1 + i;
-                }                
+                else id = 1 + i;                
             } 
-            else
-            {
-                id = 1 + i;
-            }
+            else id = 1 + i;
             DateTime date = DateTime.Now;
             string dateOfCreation = Convert.ToString(date);
             Console.WriteLine("Введите фамилию сотрудника");
@@ -75,49 +64,35 @@ namespace TaskSix
             Console.WriteLine("Введите отчество сотрудника");
             string lastName = Console.ReadLine(); 
             Console.WriteLine("Введите возраст сотрудника");
-            string age = Console.ReadLine();
+            byte age = byte.Parse(Console.ReadLine());
             string fullName = surname + " " + name + " " + lastName;
             Console.WriteLine("Введите рост сотрудника");
-            string height = Console.ReadLine();
+            byte height = byte.Parse(Console.ReadLine());
             Console.WriteLine("Введите дату рождения сотрудника в пормате ДД.ММ.ГГГГ");
-            string dayOfBirth = Console.ReadLine();
+            DateTime dayOfBirth = DateTime.Parse(Console.ReadLine());
             Console.WriteLine("Введите место рождения сотрудника");
             string placeOfBirth = Console.ReadLine();
-            return (id + "#" + dateOfCreation + "#" + fullName + "#" + age + "#" + height + "#" + dayOfBirth + "#" + placeOfBirth);
+            return (id + "#" + dateOfCreation + "#" + fullName + "#" + age + "#" + height + "#"
+                + dayOfBirth.ToShortDateString() + "#" + placeOfBirth);
         }
         /// <summary>
         /// Метод по выводу на экран данных из файла.
         /// </summary>
         static void Print()
         {
-            if (File.Exists(Path))
-            {
-                using (StreamReader sr = new StreamReader(Path))
-                {
-                    string[] result;
-                    string line = sr.ReadLine();
-                    if (line == null)
-                    {
-                        Console.WriteLine("Список сотрудников пуст");
-                    }
-                    else
-                    {
-                        while (line != null)
-                        {
-                            result = line.Split("#");
-                            for (int j = 0; j < result.Length; j++)
-                            {
-                                Console.Write(result[j] + " ");
-                            }
-                            Console.WriteLine();
-                            line = sr.ReadLine();
-                        }
-                    }
-                }
-            }
+            using StreamReader sr = new(Path);
+            string[] result;
+            string line = sr.ReadLine();
+            if (line == null) Console.WriteLine("Список сотрудников пуст");
             else
             {
-                Console.WriteLine("Список сотрудников пуст");
+                while (line != null)
+                {
+                    result = line.Split("#");
+                    for (int j = 0; j < result.Length; j++) Console.Write(result[j] + " ");
+                    Console.WriteLine();
+                    line = sr.ReadLine();
+                }
             }
         }
 
@@ -125,34 +100,20 @@ namespace TaskSix
         /// Метод по записи данных в текстовый файл
         /// </summary>
         /// <param name="newWorkerArr" массив данных из метода GetString()></param>
-        static void WriterFile(string[] newWorkerArr)
+        static void WriterFile(string[] newWorkerArr, bool fileExists)
         {
 
-            if (File.Exists(Path))
+            if (fileExists)
             {
                 string[] oldWorkerArr = File.ReadAllLines(Path);
-                using (StreamWriter sw = new StreamWriter(Path))
-                {  
-                    for (int i = 0; i < oldWorkerArr.Length; i++)
-                    {
-                        sw.WriteLine(oldWorkerArr[i]);
-                    }
-
-                    for (int i = 0; i < newWorkerArr.Length; i++)
-                    {
-                        sw.WriteLine(newWorkerArr[i]);
-                    }
-                }
+                using StreamWriter sw = new(Path);
+                for (int i = 0; i < oldWorkerArr.Length; i++) sw.WriteLine(oldWorkerArr[i]);
+                for (int i = 0; i < newWorkerArr.Length; i++) sw.WriteLine(newWorkerArr[i]);
             }
             else
             {
-                using (StreamWriter sw2 = new StreamWriter(Path))
-                {
-                    for (int i = 0; (i < newWorkerArr.Length); i++)
-                    {
-                        sw2.WriteLine(newWorkerArr[i]);
-                    }
-                }
+                using StreamWriter sw2 = new(Path);
+                for (int i = 0; i < newWorkerArr.Length; i++) sw2.WriteLine(newWorkerArr[i]);
             }
         }
     }
